3864d0b68bfb46af4b565a3ff375f0b621eed5bc
Nicolas Balaudo <nicobalaudo@gmail.com>
2026-09-01 16:12:44 -0300

    added vercel analytycs

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/fraunces/wght.css'
-import '@fontsource-variable/fraunces/wght-italic.css'
-import '@fontsource-variable/inter/wght.css'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource-variable/fraunces/wght.css";
+import "@fontsource-variable/fraunces/wght-italic.css";
+import "@fontsource-variable/inter/wght.css";
+import "./index.css";
+import { Analytics } from "@vercel/analytics/next";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
-)
+);
